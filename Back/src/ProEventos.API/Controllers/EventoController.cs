ff848be3c55994bfe5d40dc34e67c7db3f31bfdc
Wesley Daniel Martins <wesley.dmartins@outlook.com>
2022-01-30 11:35:16 -0300

    Configuração do acesso ao banco de dados sqlite

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -10,43 +11,23 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public IEnumerable<Evento> _evento = new Evento[]
-        {
-            new Evento() {
-                EventoID = 1,
-                Tema = "Angular e .NET Core 5",
-                Local = "São Paulo",
-                Lote = "1º Lote",
-                DataEvento = DateTime.Now.AddDays(5).ToString("dd/MM/yyyy"),
-                QtdPessoa = 250,
-                ImagemURL = "Foto.png"
-            },
-            new Evento() {
-                EventoID = 2,
-                Tema = "Angular e suas novidades",
-                Local = "São Paulo",
-                Lote = "2º Lote",
-                DataEvento = DateTime.Now.AddDays(15).ToString("dd/MM/yyyy"),
-                QtdPessoa = 230,
-                ImagemURL = "Foto.png"
-            }
-        };
+        private readonly DataContext _context;
 
-        public EventoController()
+        public EventoController(DataContext context)
         {
-
+            _context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(evento => evento.EventoID == id);
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoID == id);
         }
 
     }
