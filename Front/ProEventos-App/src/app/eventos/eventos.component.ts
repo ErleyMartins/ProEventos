9b224452dba298c1887c55849f7b6f4736fbb5a7
Wesley Daniel Martins <wesley.dmartins@outlook.com>
2022-02-03 19:35:31 -0300

    Terminando a view do evento

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrado: any = [];
+
+  widthImg: number = 150;
+  marginImg: number = 2;
+  isCollapsed: boolean = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista() : string{
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrado = this._filtroLista ? this.filtrarLista(this._filtroLista) : this.eventos;
+  }
+
+  filtrarLista(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter((evento: any) =>
+      evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -18,7 +41,10 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/Eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrado = this.eventos;
+      },
       error => console.error(error)
     );
   }
